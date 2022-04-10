b26588fb3673bcf4f7afd5c451d4825a8847084d
Gauthier Bossuyt <55983896+GauthierBossuyt@users.noreply.github.com>
2022-04-10 12:42:02 -0400

    docs(setup): font, icons, basic login page

---
+++ frontend/src/login/login.js
@@ -0,0 +1,31 @@
+import "./login.css";
+import Spotify_icon from "../images/SVG/Spotify.svg";
+import Logo from "../images/SVG/Logo.svg";
+import Apple_Icon from "../images/SVG/Apple.svg";
+
+function Login() {
+    return (
+        <div className="Login_page">
+            <div className="Login">
+                <img className="logo" src={Logo} alt="Boogie Logo"></img>
+                <h1>Welcome to Boogie</h1>
+                <p>
+                    To use Boogie you will need a paid subscription for any of
+                    the services below.
+                </p>
+                <div>
+                    <a id="Spotify" href="https://www.google.com">
+                        <img src={Spotify_icon} alt="Spotify icon"></img>
+                        <p>Continue using Spotify</p>
+                    </a>
+                    <a id="Apple" href="https://www.google.com">
+                        <img src={Apple_Icon} alt="Spotify icon"></img>
+                        <p>Continue using Apple Music</p>
+                    </a>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Login;
