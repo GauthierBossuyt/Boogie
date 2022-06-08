6d6977abdac34f49850f40976acb149c25bf4038
Gauthier Bossuyt <55983896+GauthierBossuyt@users.noreply.github.com>
2022-06-08 15:49:28 +0200

    fix(redirectURI): redirectURI to hosted site

--- src/login/spotify.js
+++ src/login/spotify.js
@@ -1,5 +1,5 @@
 const authEndpoint = "https://accounts.spotify.com/authorize";
-const redirectURI = "http://localhost:80/";
+const redirectURI = "http://boogie.brussels";
 const clientID = "acce0f858d36481e8c57ced906643960";
 
 const scopes = [
